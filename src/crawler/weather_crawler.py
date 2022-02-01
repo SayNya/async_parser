258fe0c36cce82267633fc5cea1a1db6ee0d1276
Alexey Lapitski <alexey.lapitski@intexsoft.by>
2022-02-01 15:05:54 +0300

    request time testing

--- src/crawler/weather_crawler.py
+++ src/crawler/weather_crawler.py
@@ -1,4 +1,6 @@
+import asyncio
 from datetime import date, timedelta
+from time import time
 
 from src.crawler.abstract_crawler import AbstractCrawler
 from src.http_client.async_client import AsyncClient
@@ -8,16 +10,22 @@ from src.storage.csv_storage import CSVStorage
 
 class WeatherCrawler(AbstractCrawler):
     def __init__(self):
-        self.HEAD_URL = 'https://meteo.by/minsk/retro/'
+        self.HEAD_URL = 'https://meteo.by/minsk/retro'
 
         self.weather_parser = WeatherParser()
         self.csv_storage = CSVStorage()
 
     async def crawl_content(self):
+        date_list = list(self.daterange(end_date=date(2003, 2, 1)))
         async with AsyncClient() as async_client:
-            pass
+            tasks = []
+            for dte in date_list:
+                task = asyncio.create_task(async_client.get_content(self.HEAD_URL + dte))
+                tasks.append(task)
+            t0 = time()
+            html_list = await asyncio.gather(*tasks)
+            print(time() - t0)
 
-
-def daterange(start_date=date(2003, 1, 1), end_date=date.today()):
-    for n in range(int((end_date - start_date).days)):
-        yield (start_date + timedelta(n)).strftime("%Y-%m-%d")
+    def daterange(self, start_date=date(2003, 1, 1), end_date=date.today()):
+        for n in range(int((end_date - start_date).days)):
+            yield (start_date + timedelta(n)).strftime(f'{self.HEAD_URL}/%Y-%m-%d')
--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ from src.crawler.weather_crawler import WeatherCrawler
 if __name__ == '__main__':
     # запускаем crawler
     weather_crawler = WeatherCrawler()
-
+    asyncio.get_event_loop().run_until_complete(weather_crawler.crawl_content())
